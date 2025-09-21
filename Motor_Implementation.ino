0af860eb7a73548791017557c9fd91a5ed5063b9
M-Nharen <nharen.m@gmail.com>
2025-09-21 22:05:53 +0530

    Update Motor_Implementation.ino

--- Motor_Implementation.ino
+++ Motor_Implementation.ino
@@ -1,13 +1,13 @@
-const int rightForward = 2
-const int rightBackward = 3
-const int leftForward = 4
-const int leftBackward = 5
+const int rightForward = 2;
+const int rightBackward = 3;
+const int leftForward = 4;
+const int leftBackward = 5;
 
 void setup() {
-pinmode(rightForward , OUTPUT);
-pinmode(rightBackward , OUTPUT);
-pinmode(leftForward , OUTPUT);
-pinmode(leftBackward , OUTPUT);
+pinMode(rightForward , OUTPUT);
+pinMode(rightBackward , OUTPUT);
+pinMode(leftForward , OUTPUT);
+pinMode(leftBackward , OUTPUT);
 }
 
 
@@ -18,7 +18,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , HIGH);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //forward*stationary
 digitalWrite(rightForward , HIGH);
@@ -26,7 +26,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , LOW);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //forward*backward
 digitalWrite(rightForward , HIGH);
@@ -34,7 +34,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , LOW);
 digitalWrite(leftBackward , HIGH);
 
-delay(2000)
+delay(2000);
 
 //stationary*Forward
 digitalWrite(rightForward , LOW);
@@ -42,7 +42,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , HIGH);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //stationary*stationary
 digitalWrite(rightForward , LOW);
@@ -50,7 +50,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , LOW);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //stationary*backward
 digitalWrite(rightForward , LOW);
@@ -58,7 +58,7 @@ digitalWrite(rightBackward , LOW);
 digitalWrite(leftForward , LOW);
 digitalWrite(leftBackward , HIGH);
 
-delay(2000)
+delay(2000);
 
 //backward*forward
 digitalWrite(rightForward , LOW);
@@ -66,7 +66,7 @@ digitalWrite(rightBackward , HIGH);
 digitalWrite(leftForward , HIGH);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //backward*stationary
 digitalWrite(rightForward , LOW);
@@ -74,7 +74,7 @@ digitalWrite(rightBackward , HIGH);
 digitalWrite(leftForward , LOW);
 digitalWrite(leftBackward , LOW);
 
-delay(2000)
+delay(2000);
 
 //backward*backward
 digitalWrite(rightForward , LOW);
