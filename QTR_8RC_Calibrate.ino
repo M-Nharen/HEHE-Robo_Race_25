28a53249d533a43e444372172121b91a3ff0c393
M-Nharen <nharen.m@gmail.com>
2025-09-21 22:03:02 +0530

    Update QTR_8RC_Calibrate.ino

--- QTR_8RC_Calibrate.ino
+++ QTR_8RC_Calibrate.ino
@@ -4,13 +4,13 @@ QTRSensors qtr;
 
 void setup()
 {
-  qtr.setypeRC();
-  qtr.setSensorPins((int[]){A0, A1, A2}, 3);
+  qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2}, 3);
 
   for (int i = 0; i < 250; i++)
   {
     qtr.calibrate();
-    delay(10);
+    delay(10);  
   }
       
 }
