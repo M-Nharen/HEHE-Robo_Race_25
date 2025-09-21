555d6dd7125f7ebece145ef86af7fa963d3fd7c6
M-Nharen <nharen.m@gmail.com>
2025-09-21 22:03:53 +0530

    Create Motor_Implementation.ino

---
+++ Motor_Implementation.ino
@@ -0,0 +1,84 @@
+const int rightForward = 2
+const int rightBackward = 3
+const int leftForward = 4
+const int leftBackward = 5
+
+void setup() {
+pinmode(rightForward , OUTPUT);
+pinmode(rightBackward , OUTPUT);
+pinmode(leftForward , OUTPUT);
+pinmode(leftBackward , OUTPUT);
+}
+
+
+void loop() {
+//forward*forward
+digitalWrite(rightForward , HIGH);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , HIGH);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//forward*stationary
+digitalWrite(rightForward , HIGH);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//forward*backward
+digitalWrite(rightForward , HIGH);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , HIGH);
+
+delay(2000)
+
+//stationary*Forward
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , HIGH);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//stationary*stationary
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//stationary*backward
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , LOW);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , HIGH);
+
+delay(2000)
+
+//backward*forward
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , HIGH);
+digitalWrite(leftForward , HIGH);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//backward*stationary
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , HIGH);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , LOW);
+
+delay(2000)
+
+//backward*backward
+digitalWrite(rightForward , LOW);
+digitalWrite(rightBackward , HIGH);
+digitalWrite(leftForward , LOW);
+digitalWrite(leftBackward , HIGH);
+}
